b25e45441b6fb05d33bc22f049502e9b18922c8c
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-02-12 11:47:39 +0100

    Add templates and layouts

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
+app.engine('hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-    res.show('index.html');
-  });
+  res.render('index');
+});
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', {layout: 'dark'});
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
+  res.render('forbidden');
 });
 
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.use((req, res) => {
-    res.status(404).show('not-found.html');
+    res.status(404).render('not-found');
   });
 
 app.listen(8000, () => {
