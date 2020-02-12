14529c0827caa9abe186e67b895b9fc1228af66f
Bartosz Iwiński <01bartosz0001@gmail.com>
2020-02-12 16:00:01 +0100

    Add form and styles in contact module

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
 app.engine('hbs', hbs());
 app.set('view engine', '.hbs');
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -29,6 +32,25 @@ app.get('/history', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message } = req.body;
+
+  if(author && sender && title && message && req.files) {
+    const photo = req.files.image.name;
+    req.files.image.mv(`./public/${photo}`, function(err) {
+      if (err) return res.status(500).send(err);
+      else {
+        res.render('contact', {isSent: true, img: photo});
+      }
+    });
+  }
+  else {
+    res.render('contact', {isError: true});
+  }
+
 });
 
 app.get('/hello/:name', (req, res) => {
